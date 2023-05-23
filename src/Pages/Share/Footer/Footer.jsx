d8819f7355b8f8d128de9d746775cb5a9275595d
abudiddikeub <abusiddikeub1@gmail.comm>
2023-05-23 12:57:46 +0600

    13th commit

--- src/Pages/Share/Footer/Footer.jsx
+++ src/Pages/Share/Footer/Footer.jsx
@@ -48,7 +48,7 @@ const Footer = () => {
      
       <footer className="footer footer-center p-4 bg-base-300 text-base-content">
   <div>
-    <p>Copyright © 2023 - All right reserved by Kiddy Zone Industries Ltd.</p>
+    <p>Copyright © 2023 - All right reserved by Toy Zone Industries Ltd.</p>
   </div>
 </footer>
     </footer>
