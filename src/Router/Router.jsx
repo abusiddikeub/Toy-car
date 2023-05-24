22908158115316fac7f5a1ac5d7310012d1fdefb
abudiddikeub <abusiddikeub1@gmail.comm>
2023-05-24 13:24:45 +0600

    19th commit

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
      },
      {
       path:'addToy',
-      element:<PrivateRouter><AddAToy></AddAToy></PrivateRouter>
+      element:<AddAToy></AddAToy>
      },
      {
      path:'allToys',
